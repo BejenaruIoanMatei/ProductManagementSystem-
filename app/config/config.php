00ebbcee2541913cb45a689dcfc603255a142970
BejenaruIoanMatei <bejematei@gmail.com>
2025-09-19 20:01:09 +0300

    3: Database connection

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Config;
+
+return [
+    'database' => [
+        'host' => '127.0.0.1',
+        'port' => 3306,
+        'dbname' => 'myapp',
+        'charset' => 'utf8mb4',
+    ],
+];
--- public/index.php
+++ public/index.php
@@ -1,7 +1,20 @@
 <?php 
 
+require __DIR__ . '/../vendor/autoload.php';
+
+use App\Config\Database;
+
 const BASE_PATH = __DIR__ . '/../';
 
 require BASE_PATH . 'app/helpers/functions.php';
 
 require base_path('app/controllers/index.php');
+
+$config = require base_path('app/config/config.php');
+
+$db = new Database($config['database']);
+$result = $db->query('select * from products where id = :id',[
+    'id' => 1
+])->find();
+
+dd($result);
