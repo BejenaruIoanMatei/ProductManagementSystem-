07939cc98f90fa42a26986edf57d5124953e10f1
BejenaruIoanMatei <bejematei@gmail.com>
2025-09-26 10:07:45 +0300

    16: Delete a product

--- app/controllers/destroy.php
+++ app/controllers/destroy.php
@@ -5,4 +5,13 @@ use App\Config\Database;
 $config = require base_path('app/config/config.php');
 $db = new Database($config['database']);
 
-dd($user);
+$product = $db->query('select * from products where id = :id',[
+    'id' => $_POST['id']
+])->findOrFail();
+
+$db->query('delete from products where id = :id',[
+    'id' => $product['id']
+]);
+
+header('location: /');
+exit();
--- app/views/show.view.php
+++ app/views/show.view.php
@@ -5,22 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Products Page</title>
 </head>
-<body>
+    <body>
 
-<?php foreach ($product as $fields): ?>
-    <?php foreach ($fields as $key => $value): ?>
-        <?php if ($key === 'id') continue; ?> 
+    <?php foreach ($product as $fields): ?>
+        <?php foreach ($fields as $key => $value): ?>
+            <?php if ($key === 'id') continue; ?> 
 
-        <?= htmlspecialchars($key) ?> - <?= htmlspecialchars($value) ?> <br>
+            <?= htmlspecialchars($key) ?> - <?= htmlspecialchars($value) ?> <br>
+        <?php endforeach; ?>
+
+        <img src="/<?= htmlspecialchars($fields['image']) ?>" 
+            alt="<?= htmlspecialchars($fields['name']) ?>" 
+            height="200">
+        <hr>
+
+        <a href="/" style="color: #27D3F5;">Go back...</a>
+
+        <form action="/product" class="mt-6" method="POST">
+            <input type="hidden" name="_method" value="DELETE">
+            <input type="hidden" name="id" value="<?= htmlspecialchars($fields['id']) ?>">
+            <button style="color: red; margin-top: 1em;">Delete</button>
+        </form>
     <?php endforeach; ?>
 
-    <img src="/<?= htmlspecialchars($fields['image']) ?>" 
-         alt="<?= htmlspecialchars($fields['name']) ?>" 
-         height="200">
-    <hr>
-<?php endforeach; ?>
 
-<a href="/" style="color: #27D3F5;">Go back...</a>
-
-</body>
+    </body>
 </html>
