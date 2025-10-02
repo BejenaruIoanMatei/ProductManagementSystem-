0fd9299f3e3711fa24497c29f8386edf304c50e7
BejenaruIoanMatei <bejematei@gmail.com>
2025-10-02 14:28:32 +0300

    21: Server-side pagination

--- app/controllers/index.php
+++ app/controllers/index.php
@@ -5,11 +5,30 @@ use App\Config\Database;
 $config = require base_path('app/config/config.php');
 
 $db = new Database($config['database']);
-$result = $db->query('select * from products')->findAllOrFail();
+
+$start = 0;
+$rows_per_page = 3;
+
+$records = $db->query('select count(*) from products')->findAllOrFail();
+
+$number_of_records = $records[0]["count(*)"];
+
+$pages = ceil($number_of_records / $rows_per_page);
+
+if(isset($_GET['page-nr']))
+{
+    $page = $_GET['page-nr'] - 1;
+    $start = $page * $rows_per_page;
+}
+
+$result = $db->query("select * from products limit $start, $rows_per_page")->findAllOrFail();
+
+// $result = $db->query('select * from products')->findAllOrFail();
 
 $heading = 'Home Page';
 
 view('index.view.php',[
     'products' => $result,
-    'heading' => $heading
+    'heading' => $heading,
+    'pages' => $pages
 ]);
--- app/views/index.view.php
+++ app/views/index.view.php
@@ -2,27 +2,86 @@
 <?php require base_path('app/views/partials/head.html') ?>
 
 <body>
+    <?php require base_path('app/views/partials/nav.php') ?>
+    <?php require base_path('app/views/partials/banner.php') ?>
 
-<?php require base_path('app/views/partials/nav.php') ?>
-<?php require base_path('app/views/partials/banner.php') ?>
+    <?php foreach ($products as $product): ?>
+        <?php foreach ($product as $key => $value): ?>
+            <?php if ($key === 'name') : ?>
+                <a href="/product?id=<?= $product['id'] ?>">Link to Product</a>
+                <br>
+            <?php endif ?>
 
-<?php foreach ($products as $product): ?>
-    <?php foreach ($product as $key => $value): ?>
-        <?php if ($key === 'id') continue; ?> 
-        <?php if ($key === 'name') : ?>
-            <a href="/product?id=<?= $product['id'] ?>">Link to Product</a>
-            <br>
-        <?php endif ?>
+            <?= htmlspecialchars($key) ?> - <?= htmlspecialchars($value) ?> <br>
+        <?php endforeach; ?>
 
-        <?= htmlspecialchars($key) ?> - <?= htmlspecialchars($value) ?> <br>
+        <img src="/<?= htmlspecialchars($product['image']) ?>" 
+            alt="<?= htmlspecialchars($product['name']) ?>" 
+            height="200">
+        <hr>
     <?php endforeach; ?>
 
-    <img src="/<?= htmlspecialchars($product['image']) ?>" 
-         alt="<?= htmlspecialchars($product['name']) ?>" 
-         height="200">
-    <hr>
-<?php endforeach; ?>
 
 
+    <div class="page-info">
+        <?php 
+            if(!isset($_GET['page-nr']))
+            {
+                $page = 1;
+            }else{
+                $page = $_GET['page-nr'];
+            }
+        ?>
+
+        Showing <?= $page ?> of <?= $pages ?>
+    </div>
+
+    <div class="pagination">
+        <a href="?page-nr=1">First</a>
+
+        <?php
+            if(isset($_GET['page-nr']) and $_GET['page-nr'] > 1)
+            {
+                ?>
+                    <a href="?page-nr=<?= $_GET['page-nr'] - 1?>">Previous</a>
+                <?php
+            }else{
+                ?>
+                    <a>Previous</a>
+                <?php
+            }
+        ?>
+        <div class="page-numbers">
+            <?php 
+                for ($counter = 1; $counter <= $pages; $counter ++)
+                {
+                    ?>
+                        <a href="?page-nr=<?= $counter ?>"><?= $counter ?></a>
+                    <?php
+                }
+            ?>
+        </div>
+
+        <?php
+            if(!isset($_GET['page-nr']))
+            {
+                ?>
+                    <a href="?page-nr=2">Next</a>
+                <?php
+            }else{
+                if($_GET['page-nr'] >= $pages)
+                {
+                    ?>
+                        <a>Next</a>
+                    <?php
+                }else{
+                    ?>
+                        <a href="?page-nr=<?= $_GET['page-nr'] + 1 ?>">Next</a>
+                    <?php
+                }
+            }
+        ?>
+        <a href="?page-nr=<?= $pages ?>">Last</a>
+    </div>
 </body>
 </html>
